2e5ce8d9aeb260ce33bc611254b1a4e849768664
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-09-10 14:07:03 +0100

    fix blur background toggle

--- js/index.js
+++ js/index.js
@@ -2,8 +2,7 @@ const navToggle = (e) => {
     const icons = e.querySelectorAll("img");
     const background = e.parentElement.parentElement.querySelector(".mobile-bg-header");
     const list = e.parentElement.querySelector("ul");
-
-    console.log(background);
+    
     background.classList.toggle("display-bg");
     icons[0].classList.toggle("display");
     icons[1].classList.toggle("display-toggle");
