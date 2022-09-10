b1c1406eb7a42828a510985a67064da156a62b68
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-09-10 14:11:25 +0100

    overflow background toggle menu

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ const navToggle = (e) => {
     const icons = e.querySelectorAll("img");
     const background = e.parentElement.parentElement.querySelector(".mobile-bg-header");
     const list = e.parentElement.querySelector("ul");
-    
+
+    document.querySelector("body").classList.toggle("scroll");
     background.classList.toggle("display-bg");
     icons[0].classList.toggle("display");
     icons[1].classList.toggle("display-toggle");
