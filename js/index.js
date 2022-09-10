128393d6b949573e732fdcb8765c6162ba6052e6
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-09-10 03:36:25 +0100

    finish active state

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 const navToggle = (e) => {
     const icons = e.querySelectorAll("img");
+    const background = e.parentElement.parentElement.querySelector(".mobile-bg-header");
     const list = e.parentElement.querySelector("ul");
 
+    console.log(background);
+    background.classList.toggle("display-bg");
     icons[0].classList.toggle("display");
     icons[1].classList.toggle("display-toggle");
     list.classList.toggle("display-list");
